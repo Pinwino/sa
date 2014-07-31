eaf7b5a77da047e0d6b5e15afd7ee0d2d02e9d18
Jose Jimenez <ppjm@correo.ugr.es>
2014-07-31 22:57:58 +0200

    calibration.c, fine-delay.mk: fine-delay-sw with my patch
    debugger_sw/calibration.c: Gone!
    debugger_sw/fmc-delay/fine-delay.mk: Updated
    
    Signed-off-by:  <ppjm@correo.ugr.es>

--- debugger_sw/fmc-delay/fine-delay.mk
+++ debugger_sw/fmc-delay/fine-delay.mk
@@ -1,26 +1,26 @@
 #kernel files
 FINE_DEL_KERNEL_DIR = fmc-delay/fine-delay-sw/kernel
 obj-$(CONFIG_FINE_DEL_NODE) += \
-         $(FINE_DEL_KERNEL_DIR)/spi.o \
-         $(FINE_DEL_KERNEL_DIR)/gpio.o \
-         $(FINE_DEL_KERNEL_DIR)/pll.o \
-         $(FINE_DEL_KERNEL_DIR)/onewire.o \
-         $(FINE_DEL_KERNEL_DIR)/time.o \
-         $(FINE_DEL_KERNEL_DIR)/i2c.o \
-         $(FINE_DEL_KERNEL_DIR)/calibrate.o \
-         $(FINE_DEL_KERNEL_DIR)/acam.o \
-         calibration.o #patch
-	     
+	$(FINE_DEL_KERNEL_DIR)/spi.o \
+	$(FINE_DEL_KERNEL_DIR)/gpio.o \
+	$(FINE_DEL_KERNEL_DIR)/pll.o \
+	$(FINE_DEL_KERNEL_DIR)/onewire.o \
+	$(FINE_DEL_KERNEL_DIR)/time.o \
+	$(FINE_DEL_KERNEL_DIR)/i2c.o \
+	$(FINE_DEL_KERNEL_DIR)/calibrate.o \
+	$(FINE_DEL_KERNEL_DIR)/acam.o \
+	$(FINE_DEL_KERNEL_DIR)/calibration.o
+
 #commands
 FINE_DEL_CMP_DIR = fmc-delay/fine-delay-cmds
 obj-$(CONFIG_FINE_DEL_NODE) += \
-	     $(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_pulse.o \
-	     $(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_term.o \
-         $(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_status.o \
-	     $(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_time.o \
-	     $(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_temp.o \
-	     $(FINE_DEL_CMP_DIR)/tools-util.o \
-	     $(FINE_DEL_CMP_DIR)/fdelay-output.o	     
+	$(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_pulse.o \
+	$(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_term.o \
+	$(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_status.o \
+	$(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_time.o \
+	$(FINE_DEL_CMP_DIR)/cmd_fmc_fdelay_temp.o \
+	$(FINE_DEL_CMP_DIR)/tools-util.o \
+	$(FINE_DEL_CMP_DIR)/fdelay-output.o     
 
 #main
 FINE_DEL_DIR = fmc-delay
